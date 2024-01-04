110c1226c6ed5c5c4a3bac7293b0bd76ffb2e3f4
Yogesh Kumar <kumaryogesh8490@gmail.com>
2024-01-04 12:18:13 +0530

    added mui header drawer to show menu in sm screen.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,3 +1,94 @@
+// /* eslint-disable react/prop-types */
+// import { Button } from "antd";
+// import "./Header.css";
+// import { useNavigate } from "react-router-dom";
+// import ThemeBtn from '../ThemeBtn/themebtn';
+// import useTheme from "../../contexts/theme";
+
+// const Header = ({ children }) => {
+//     const navigate = useNavigate();
+//     const { themeMode } = useTheme();
+
+
+//     const home = () => {
+//         navigate("/products");
+//     };
+
+//     const explore = () => {
+//         navigate("/products");
+//     };
+
+//     const register = () => {
+//         navigate("/register");
+//     };
+
+//     const login = () => {
+//         navigate("/login");
+//     };
+
+//     const logout = () => {
+//         for (const key in localStorage) {
+//             if (key !== 'themeMode') {
+//                 localStorage.removeItem(key);
+//             }
+//         }
+//         navigate("/");
+//     };
+
+//     return (
+//         <div className={`header ${themeMode}`} >
+//             {/* Shows QBuyDot title image */}
+//             <div className="header-title" onClick={home} >
+//                 <img src={themeMode === 'dark' ? 'QBuyDotLogo-newDarkMode.svg' : 'QBuyDotLogo-newLightMode.svg'} alt="QbuyDot-icon"></img>
+//             </div>
+
+//             {children}
+
+//             {/* Display links based on if the user's logged in or not */}
+//             <div className="header-action">
+//                 {localStorage.getItem("username") ? (
+//                     <>
+//                         <img
+//                             src="avatar.png"
+//                             alt="profile"
+//                             className="profile-image"
+//                         ></img>
+
+//                         <div className="header-info">
+//                             {localStorage.getItem("username")}
+//                         </div>
+
+//                         <Button type="primary" onClick={logout}>
+//                             Logout
+//                         </Button>
+//                         <ThemeBtn />
+//                     </>
+//                 ) : (
+//                     <>
+//                         <div className="header-link" onClick={explore}>
+//                             Explore
+//                         </div>
+
+//                         <div className="header-link" onClick={login}>
+//                             Login
+//                         </div>
+
+//                         <div className="header-link">
+//                             <Button type="primary" onClick={register}>
+//                                 Register
+//                             </Button>
+//                             <ThemeBtn />
+//                         </div>
+//                     </>
+//                 )}
+
+//             </div>
+//         </div >
+//     );
+// };
+
+// export default Header;
+
 /* eslint-disable react/prop-types */
 import { Button } from "antd";
 import "./Header.css";
